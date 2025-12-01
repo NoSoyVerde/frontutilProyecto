6ec80ed380d367cc0ae0db4ff94cd9c7accaa561
Olga <157001903+OlgaGarciaQuintana@users.noreply.github.com>
2025-12-01 20:40:14 +0100

    Frontutil-Olga-Reto4 (#7)
    * añadir el home
    
    * mejorado el home
    
    * model terminado
    
    * terminado el service
    
    * todos los componentes creados
    
    * ficheros añadidos
    
    * cambiando nombres rutas y funciona
    
    * admin edit terminado
    
    * todo puesto con lo mio
    
    * faltan mas cambios
    
    * ver pq no me sale progreso
    
    * funciona
    
    * boton volver
    
    * que vaya al home
    
    * PORFIN EL BOTON DEL POST ID
    
    * botones al objetivo e inicio
    
    * link actualizado
    
    * otro mas
    
    * todas las rutas bien
    
    * añadir fecha final
    
    * que se pueda editar la fecha
    
    * barra de progreso actualizada
    
    * solucionado
    
    * mostrar progreso
    
    * generar frases aleatorias
    
    * formateado, para que se vea bonito
    
    * todo feliz
    
    ---------
    
    Co-authored-by: Rafael Aznar <rafaaznar@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,13 @@ import { RoutedUserView } from './component/blog/routed-user-view/routed-user-vi
 import { RoutedAdminEdit } from './component/blog/routed-admin-edit/routed-admin-edit';
 import { RoutedAdminNew } from './component/blog/routed-admin-new/routed-admin-new';
 import { RoutedAdminRemove } from './component/blog/routed-admin-remove/routed-admin-remove';
+import { RoutedUserPlistGarcia } from './component/garcia/routed-user-plistGarcia/Garciarouted-user-plist';
+import { RoutedUserViewGarcia } from './component/garcia/routed-user-viewGarcia/Garciarouted-user-view';
+import { RoutedAdminPlistGarcia } from './component/garcia/routed-admin-plistGarcia/Garciarouted-admin-plist';
+import { RoutedAdminViewGarcia } from './component/garcia/routed-admin-viewGarcia/Garciarouted-admin-view';
+import { RoutedAdminNewGarcia } from './component/garcia/routed-admin-newGarcia/Garciarouted-admin-new';
+import { RoutedAdminEditGarcia } from './component/garcia/routed-admin-editGarcia/Garciarouted-admin-edit';
+import { RoutedAdminRemoveGarcia } from './component/garcia/routed-admin-removeGarcia/Garciarouted-admin-remove';
 import { FernandezRoutedAdminPlist } from './component/fernandez-idea/routed-admin-plist/routed-admin-plist';
 import { FernandezRoutedAdminView } from './component/fernandez-idea/routed-admin-view/routed-admin-view';
 import { FernandezRoutedAdminEdit } from './component/fernandez-idea/routed-admin-edit/routed-admin-edit';
@@ -105,6 +112,13 @@ export const routes: Routes = [
   { path: 'blog/new', component: RoutedAdminNew },
   { path: 'blog/edit/:id', component: RoutedAdminEdit },
   { path: 'blog/remove/:id', component: RoutedAdminRemove },
+  { path: 'garcia', component: RoutedUserPlistGarcia},
+  { path: 'garcia/post/:id', component: RoutedUserViewGarcia },
+  { path: 'garcia/plist', component: RoutedAdminPlistGarcia },
+  { path: 'garcia/view/:id', component: RoutedAdminViewGarcia },
+  { path: 'garcia/new', component: RoutedAdminNewGarcia },
+  { path: 'garcia/edit/:id', component: RoutedAdminEditGarcia },
+  { path: 'garcia/remove/:id', component: RoutedAdminRemoveGarcia }
 
   { path: 'castanyera', component: CastanyeraRoutedUserPlist },
   { path: 'castanyera/post/:id', component: CastanyeraRoutedUserView },
